61f0113860a52067ae94585ad76eebdd508295ab
Klimov Kirill <kirill.klimov@is.ru>
2022-09-26 19:23:36 +0300

    meg

---
+++ cpp_mod_0/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknotts <jknotts@student.21-school>       +#+  +:+       +#+         */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/26 22:43:52 by jknotts           #+#    #+#             */
+/*   Updated: 2022/09/26 23:35:44 by jknotts          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstring>
+
+void meg(int argc, char **argv)
+{
+    while(--argc)
+    {
+        argv++;
+        for(size_t i = 0; i < strlen(*argv); i++)
+            (*argv)[i] = std::toupper((*argv)[i]);
+        std::cout << *argv << " ";
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return (0);
+    } else
+        meg(argc, argv);
+    std::cout << std::endl;
+    return (0);
+}
