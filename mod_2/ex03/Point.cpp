02a37922f95fd43b99414b634a0ca19e54b149c6
jknotts <kirill.klimov.94@gmail.com>
2022-10-05 23:46:41 +0300

    m2/ex03 fin

--- mod_2/ex03/Point.cpp
+++ mod_2/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By:  <jknotts@student.21-school>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 22:47:36 by jknotts           #+#    #+#             */
-/*   Updated: 2022/10/05 22:48:53 by                  ###   ########.fr       */
+/*   Updated: 2022/10/05 23:21:37 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Point.h"
@@ -14,4 +14,38 @@
 Point::Point()
 {
 
+}
+
+Point::Point(float x, float y) : _x(x), _y(y)
+{
+
+}
+
+Point::Point(const Point &rhs)
+{
+	*this = rhs;
+}
+
+Point::~Point()
+{
+
+}
+
+Point &Point::operator=(const Point &rhs)
+{
+	if (this == &rhs)
+		return *this;
+	_x = rhs.getX();
+	_y = rhs.getY();
+	return *this;
+}
+
+Fixed Point::getX() const
+{
+	return _x;
+}
+
+Fixed Point::getY() const
+{
+	return _y;
 }
--- mod_2/ex03/bsp.cpp
+++ mod_2/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By:  <jknotts@student.21-school>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 22:28:03 by jknotts           #+#    #+#             */
-/*   Updated: 2022/10/05 22:39:26 by                  ###   ########.fr       */
+/*   Updated: 2022/10/05 23:29:03 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	pl_a = (a.getX() - point.getX()) * (b.getY() - a.getY()) - (b.getX() - a.getX()) * (a.getY() - point.getY());
 	pl_b = (b.getX() - point.getX()) * (c.getY() - b.getY()) - (c.getX() - b.getX()) * (b.getY() - point.getY());
 	pl_c = (c.getX() - point.getX()) * (a.getY() - c.getY()) - (a.getX() - c.getX()) * (c.getY() - point.getY());
+
+
 
 	if ((pl_a > 0 && pl_b > 0 && pl_c > 0) || (pl_a < 0 && pl_b < 0 && pl_c < 0))
 		return true;
